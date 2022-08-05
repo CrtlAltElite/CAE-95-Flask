55b333b5518b399e32e7bd85715a3ff2e5f3b6a0
kevin beier <kevinb@codingtemple.com>
2022-08-05 12:48:03 -0400

    end of flask project

--- app/blueprints/api/shop_routes.py
+++ app/blueprints/api/shop_routes.py
@@ -1,9 +1,14 @@
 from . import bp as api
 from app.blueprints.auth.auth import token_auth
-from flask import request, make_response, g, abort
+from flask import request, make_response, g, abort, jsonify
 from .models import *
 from helpers import require_admin
 
+############
+##
+##  CATEGORY API ROUTES
+##
+############
 
 # Get all cats
 @api.get('/category')
@@ -56,3 +61,90 @@ def delete_category(id):
         abort(404)
     cat.delete()
     return make_response(f"Category {id} deleted", 200)
+
+
+#############
+##
+##  ITEM API ROUTES
+##
+############
+
+
+# Get All items from the shop
+
+@api.get('/item')
+def get_items():
+    # Get all the items from the database
+    items = Item.query.all()
+    # convert the items to dicts
+    items_dicts=[item.to_dict() for item in items]
+    # return the response
+    return make_response({"items":items_dicts},200)
+
+# Get an item by its id
+@api.get('/item/<int:id>')
+def get_item(id):
+    # look it up in the db
+    item=Item.query.get(id)
+    # Item.query.filter(Item.id==id).first()
+    # Item.query.filter_by(id=id).first()
+    if not item:
+        abort(404)
+    item_dict = item.to_dict()
+    return make_response(item_dict, 200)
+
+# Get all items in a Category by cat id
+@api.get('/item/category/<int:id>')
+def get_items_by_cat(id):
+    cat = Category.query.get(id)
+    if not cat:
+        abort(404)
+    all_items_in_cat = [item.to_dict() for item in cat.products]
+    return make_response({"items":all_items_in_cat}, 200)
+
+# Create a new Item
+# {
+# 'name' : String,
+#  'desc' : string,
+#  'price': float,
+#  'img': string,
+#  'category_id': int
+#  }
+@api.post("/item")
+@token_auth.login_required()
+@require_admin
+def post_item():
+    # Get the payload
+    item_dict=request.get_json()
+    # Ensure the payload has all the appro. values
+    if not all(key in item_dict for key in ('name', 'desc', 'price', 'img', 'category_id')):
+        abort(400)
+    item = Item()
+    item.from_dict(item_dict)
+    item.save()
+    return make_response(f'{item.name} was created', 200)
+
+#id of the item they changing and the information they want to change
+@api.put("/item/<int:id>")
+@token_auth.login_required()
+@require_admin
+def put_item(id):
+    # Get the payload
+    item_dict=request.get_json()
+    item = Item.query.get(id)
+    if not item:
+        abort(404)
+    item.from_dict(item_dict)
+    item.save()
+    return make_response(f'{item.name} was updated', 200)
+
+# Delete an Item
+@api.delete('/item/<int:id>')
+@token_auth.login_required()
+@require_admin
+def delete_item(id):
+    item_to_del = Item.query.get(id)
+    if not item_to_del:
+        abort(404)
+    item_to_del.delete()
+    return make_response(f'item {id} was deleted', 200)
